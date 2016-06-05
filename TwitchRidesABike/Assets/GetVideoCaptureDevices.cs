f817d0a5cbbb8ba6b00c55fd9017e2da299e486b
shane <haroboy876@gmail.com>
2016-06-04 17:11:01 -0700

    add initial get video capture devices script

---
+++ TwitchRidesABike/Assets/GetVideoCaptureDevices.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+	// Gets the list of devices and prints them to the console.
+	void Start() {
+		WebCamDevice[] devices = WebCamTexture.devices;
+		for( var i = 0 ; i < devices.Length ; i++ )
+			Debug.Log(devices[i].name);        
+	}
+}
